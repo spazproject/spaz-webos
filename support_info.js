59ada2dfa0adaa0686a724e65d8bab53b45769e8
Ed Finkler <funkatron@gmail.com>
2009-05-06 17:16:13 -0400

    - removed MORE http:// prefixes in support info

--- support_info.js
+++ support_info.js
@@ -5,12 +5,12 @@ _APP_PublisherName = 'Funkatron Productions'
 _APP_Copyright = '&copy; Copyright 2009 Funkatron Productions.'
 
 // At least one of these three is required
-_APP_Support_URL = 'http://funkatron.com/spaz-webos'   // label = ÒSupport WebsiteÓ
+_APP_Support_URL = 'funkatron.com/spaz-webos'   // label = ÒSupport WebsiteÓ
 _APP_Support_Email = 'spaz-webos@funkatron.com'        // label = ÒSend EmailÓ
 // _APP_Support_Phone = '555-555-5555'        // label = _APP_Support_Phone
 
 // Optional
-_APP_Publisher_URL = 'http://www.funkatron.com' // label = _APP_PublisherName + ÒWebsiteÓ
+_APP_Publisher_URL = 'funkatron.com' // label = _APP_PublisherName + ÒWebsiteÓ
 _APP_Help_Resource = [
 					 	{ type: 'web', label: 'Wiki', url: 'funkatron.com/spaz-webos/wiki'},
 					 	{ type: 'web', label: 'User Discussion Group', url: 'funkatron.com/spaz-webos/group'},
