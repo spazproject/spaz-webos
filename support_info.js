d63181beba511bd5d0311dca2c4e6f6c00ba8939
Ed Finkler <funkatron@gmail.com>
2009-06-11 20:35:15 -0400

    Revert "Revert "- refactoring of handleCommand in scene.js to move navigation commands into stageAssistant""
    This reverts commit 101d893852ca8997e4bcce9f96a0a27d1d5c620f.

--- support_info.js
+++ support_info.js
@@ -1,16 +1,16 @@
 // Required
-_APP_Name = 'Spaz'
-_APP_VersionNumber = '0.4.3'
-_APP_PublisherName = 'Funkatron Productions'
-_APP_Copyright = '&copy; Copyright 2009 Funkatron Productions.'
+_APP_Name = 'Spaz';
+_APP_VersionNumber = '0.4.3';
+_APP_PublisherName = 'Funkatron Productions';
+_APP_Copyright = '&copy; Copyright 2009 Funkatron Productions.';
 
 // At least one of these three is required
-_APP_Support_URL = 'funkatron.com/spaz-webos'   // label = ÒSupport WebsiteÓ
-_APP_Support_Email = 'spaz-webos@funkatron.com'        // label = ÒSend EmailÓ
+_APP_Support_URL = 'funkatron.com/spaz-webos';   // label = ÒSupport WebsiteÓ
+_APP_Support_Email = 'spaz-webos@funkatron.com';        // label = ÒSend EmailÓ
 // _APP_Support_Phone = '555-555-5555'        // label = _APP_Support_Phone
 
 // Optional
-_APP_Publisher_URL = 'funkatron.com' // label = _APP_PublisherName + ÒWebsiteÓ
+_APP_Publisher_URL = 'funkatron.com'; // label = _APP_PublisherName + ÒWebsiteÓ
 _APP_Help_Resource = [
 					 	{ type: 'web', label: 'Wiki', url: 'funkatron.com/spaz-webos/wiki'},
 					 	{ type: 'web', label: 'User Discussion Group', url: 'funkatron.com/spaz-webos/group'},
