3ad2d8257885392476dd6e77c4692f27bc178220
Ed Finkler <coj@funkatron.com>
2010-09-17 01:58:42 -0400

    switched a ton of sch.error()s to sch.debug()

--- app/models/tweets.js
+++ app/models/tweets.js
@@ -31,11 +31,11 @@ var Tweets = function(opts) {
 		var matches = [];
 		this.each(function(record, index) {
 			if (is(record)) {
-				sch.error("found match "+record.key);
+				sch.debug("found match "+record.key);
 				matches.push(record);
 			}
 		}, function(count) {
-			sch.error('Firing callback on array of matches('+count+')');
+			sch.debug('Firing callback on array of matches('+count+')');
 			cb(matches);
 		});
 	};
@@ -51,12 +51,12 @@ Tweets.prototype.maxBucketSize = 20000;
 
 Tweets.prototype._init  = function(replace) {
 	if (replace === true) {
-		sch.error('REPLACING DEPOT!!!!!!!!!!!=======================');
+		sch.debug('REPLACING DEPOT!!!!!!!!!!!=======================');
 		this.bucket.nuke();
 		this.dm_bucket.nuke();
 		this.user_bucket.nuke();
 	} else {
-		sch.error('NOT REPLACING DEPOT!!!!!!!!!!====================');
+		sch.debug('NOT REPLACING DEPOT!!!!!!!!!!====================');
 	}
 };
 
@@ -73,19 +73,19 @@ Tweets.prototype.get    = function(id, isdm, onSuccess, onFailure) {
 	bucket.get(id,
 		function(data) { // wrapper for the passed onSuccess
 			if (!data) {
-				sch.error("Couldn't retrieve id "+id+"; getting remotely");
+				sch.debug("Couldn't retrieve id "+id+"; getting remotely");
 				that.getRemote(
 					id,
 					isdm,
 					function(data) {
-						sch.error('saving remotely retrieved message');
+						sch.debug('saving remotely retrieved message');
 						bucket.save(data);
 						onSuccess(data);
 					},
 					onFailure
 				);
 			} else {
-				sch.error("Retrieved id "+id+" from lawnchair bucket");
+				sch.debug("Retrieved id "+id+" from lawnchair bucket");
 				onSuccess(data);
 			}
 		},
@@ -104,33 +104,33 @@ Tweets.prototype.save   = function(object, onSuccess, onFailure) {
 	
 	object.key = objid;
 
-	sch.error("Saving object "+sch.enJSON(object));
+	sch.debug("Saving object "+sch.enJSON(object));
 	
 	
-	sch.error("Saving id "+objid);
+	sch.debug("Saving id "+objid);
 	if (!object.SC_is_dm) {
-		sch.error("Saving TWEET "+objid);
+		sch.debug("Saving TWEET "+objid);
 		this.bucket.save(object);
 		if (object.user) {
-			sch.error("Saving user "+object.user.id);
+			sch.debug("Saving user "+object.user.id);
 			this.saveUser(object.user);			
 		} else {
-			sch.error('Tweet '+objid+' did not have a user object');
+			sch.debug('Tweet '+objid+' did not have a user object');
 		}
 	} else {
-		sch.error("Saving DM "+objid);
+		sch.debug("Saving DM "+objid);
 		this.dm_bucket.save(object);
 		if (object.sender) {
-			sch.error("Saving user "+object.sender.id);
+			sch.debug("Saving user "+object.sender.id);
 			this.saveUser(object.sender);
 		} else {
-			sch.error('Tweet '+objid+' did not have a sender object');
+			sch.debug('Tweet '+objid+' did not have a sender object');
 		}
 		if (object.recipient) {
-			sch.error("Saving user "+object.recipient.id);
+			sch.debug("Saving user "+object.recipient.id);
 			this.saveUser(object.recipient);
 		} else {
-			sch.error('Tweet '+objid+' did not have a recipient object');
+			sch.debug('Tweet '+objid+' did not have a recipient object');
 		}
 	}
 };
@@ -156,22 +156,22 @@ Tweets.prototype.getUser = function(id, onSuccess, onFailure) {
 	var that = this;
 	var screen_name;
 	
-	sch.error('passed id is "'+id+'"');
+	sch.debug('passed id is "'+id+'"');
 	
 	var onDataSuccess = function(data) { // wrapper for the passed onSuccess
 		if (!data) {
-			sch.error("Couldn't retrieve id "+id+"; getting remotely");
+			sch.debug("Couldn't retrieve id "+id+"; getting remotely");
 			that.getRemoteUser(
 				id,
 				function(data) {
-					sch.error('saving remotely retrieved user');
+					sch.debug('saving remotely retrieved user');
 					that.saveUser(data);
 					onSuccess(data);
 				},
 				onFailure
 			);
 		} else {
-			sch.error("Retrieved user id "+id+" from lawnchair bucket");
+			sch.debug("Retrieved user id "+id+" from lawnchair bucket");
 			onSuccess(data);
 		}
 	};
@@ -180,9 +180,9 @@ Tweets.prototype.getUser = function(id, onSuccess, onFailure) {
 		if the id starts with a '@', we have a screen_name
 	*/
 	if (id.indexOf('@') === 0) {
-		sch.error('we have a screen name');
+		sch.debug('we have a screen name');
 		screen_name = id.slice(1);
-		sch.error('screen name is '+screen_name);
+		sch.debug('screen name is '+screen_name);
 		this.user_bucket.match(
 			function(r) {
 				if (r.screen_name == screen_name) {
@@ -289,7 +289,7 @@ Tweets.prototype.getBucket = function(isdm) {
 Tweets.prototype.getRemote = function(id, isdm, onSuccess, onFailure) {
 	this.initSpazTwit();
 	
-	sch.error("getting message id "+id+" remotely!!");
+	sch.debug("getting message id "+id+" remotely!!");
 	
 	if (isdm) {
 		thisA.showAlert($L('There was an error retrieving this direct message from cache'));
@@ -301,7 +301,7 @@ Tweets.prototype.getRemote = function(id, isdm, onSuccess, onFailure) {
 Tweets.prototype.getRemoteUser = function(id, onSuccess, onFailure) {
 	this.initSpazTwit();
 	
-	sch.error("getting user id "+id+" remotely!!");
+	sch.debug("getting user id "+id+" remotely!!");
 	
 	this.twit.getUser(id, onSuccess, onFailure);
 };
