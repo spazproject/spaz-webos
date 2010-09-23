2303434653685cb7c1c2389b88353e5d8e4b7a65
Ed Finkler <coj@funkatron.com>
2010-09-23 14:39:18 -0400

    App title now changes in start scene according to appinfo.json

--- app/assistants/start-assistant.js
+++ app/assistants/start-assistant.js
@@ -89,6 +89,7 @@ StartAssistant.prototype.activate = function(argFromPusher) {
 	/* put in event handlers here that should only be in effect when this scene is active. For
 	   example, key handlers that are observing the document */
 
+	jQuery('#app-title').text(Mojo.appInfo.title);
 	jQuery('#app-version').text("v"+Mojo.appInfo.version);
 	
 	/*
