c27f2430e89d253c93fd304d54ac75e59bc6de8f
Ed Finkler <coj@funkatron.com>
2011-02-22 21:37:22 -0500

    added initial work on OSXX theme (which sorta sucks ass right now)

--- app/views/themes.js
+++ app/views/themes.js
@@ -7,6 +7,10 @@ var AppThemes = {
 		"stylesheet":"Clean.css",
 		"palmtheme":'light'
 	},
+	"OSXX":{
+		"stylesheet":"OSXX.css",
+		"palmtheme":'light'
+	},
 	"Koto":{
 		"stylesheet":"Koto.css",
 		"palmtheme":'dark'
