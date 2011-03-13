474471147706b1925176f570a5c62ddfde461417
Ed Finkler <coj@funkatron.com>
2011-03-13 19:54:42 -0400

    fix themes file

--- app/views/themes.js
+++ app/views/themes.js
@@ -2,5 +2,21 @@ var AppThemes = {
 	"Dreadnaught":{
 		"stylesheet":"Dreadnaught.css",
 		"palmtheme":'dark'
+	},
+	"Clean":{
+		"stylesheet":"Clean.css",
+		"palmtheme":'light'
+	},
+	"OSXX":{
+		"stylesheet":"OSXX.css",
+		"palmtheme":'dark'
+	},
+	"Koto":{
+		"stylesheet":"Koto.css",
+		"palmtheme":'dark'
+	},
+	"Preware":{
+		"stylesheet":"Preware.css",
+		"palmtheme":'light'
 	}
 };
