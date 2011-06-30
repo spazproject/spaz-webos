db020c1a4f2d904f73f6ad12f91141a5329835ab
Ed Finkler <coj@funkatron.com>
2011-06-30 14:42:15 -0400

    removed extra themes

--- app/views/themes.js
+++ app/views/themes.js
@@ -2,21 +2,21 @@ var AppThemes = {
 	"Dreadnaught":{
 		"stylesheet":"Dreadnaught.css",
 		"palmtheme":'dark'
-	},
-	"Clean":{
-		"stylesheet":"Clean.css",
-		"palmtheme":'light'
-	},
-	"OSXX":{
-		"stylesheet":"OSXX.css",
-		"palmtheme":'dark'
-	},
-	"Koto":{
-		"stylesheet":"Koto.css",
-		"palmtheme":'dark'
-	},
-	"Preware":{
-		"stylesheet":"Preware.css",
-		"palmtheme":'light'
-	}
+	}// ,
+	// 	"Clean":{
+	// 		"stylesheet":"Clean.css",
+	// 		"palmtheme":'light'
+	// 	},
+	// 	"OSXX":{
+	// 		"stylesheet":"OSXX.css",
+	// 		"palmtheme":'dark'
+	// 	},
+	// 	"Koto":{
+	// 		"stylesheet":"Koto.css",
+	// 		"palmtheme":'dark'
+	// 	},
+	// 	"Preware":{
+	// 		"stylesheet":"Preware.css",
+	// 		"palmtheme":'light'
+	// 	}
 };
